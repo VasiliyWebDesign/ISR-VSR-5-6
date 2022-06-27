5bee35cde2148d5aebf7feff0c9afd2dbbd8314b
Третьяк Глеб Игоревич <60621582+VasiliyWebDesign@users.noreply.github.com>
2022-06-27 15:47:30 +0300

    Add files via upload

---
+++ ПРОГ-6/ISR2-2.py
@@ -0,0 +1,11 @@
+# Задание: разработать программу, позволяющую генерировать
+# уникальные идентификаторы: UUID (universally unique identifier).
+# Структура UUID — на усмотрение студента.
+
+
+
+import uuid
+
+domain = str(input("Введите домен: "))
+generated_uuid = uuid.uuid3(uuid.NAMESPACE_DNS, domain)
+print(generated_uuid)
